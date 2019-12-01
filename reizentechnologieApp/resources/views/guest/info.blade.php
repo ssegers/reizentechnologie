0733c783f56025ff2a2a409986ef043bcdc5e358
Pieter Coenen <r0668811@ucll.be>
2019-12-01 10:52:57 +0100

    opmaak info pagina

--- reizentechnologieApp/resources/views/guest/info.blade.php
+++ reizentechnologieApp/resources/views/guest/info.blade.php
@@ -4,16 +4,41 @@
     .carousel-inner > .item > img, .carousel-inner > .item > a > img{
     margin:auto;
 }
-   </style>
+
+textarea{
+    margin-top: 20px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
+}
+
+h1{
+    margin-top: 20px;
+}
+
+input{
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 20px;
+}
+
+.container{
+    text-align: center;
+}
+
+</style>
 @endsection
 @section('content')
 <div class="container">
+<h1>Algemene info</h1>
+<hr />
 <form method="POST" class="htmlEditor" action="/admin/info">
     <div>
         <textarea cols="80" rows="12" id="info_content" name="info_content"> <?php echo $info_content; ?>
         </textarea>
         <input type="submit" value="Opslaan" name="action"/>
-         <input type="submit" value="Annuleren" name="action"/>
+        <input type="submit" value="Annuleren" name="action"/>
     </div>
 </form>
 </div>
